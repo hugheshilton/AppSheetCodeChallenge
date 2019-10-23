eecbe9ffe228f52510ffa703bad0b2a5f7607b09
lordofthewarcry@gmail.com <lordofthewarcry@gmail.com>
2019-10-22 21:02:29 -0700

    Created a UI that shows the people retrieved.

---
+++ AppSheetChallenge.UI/Command.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace AppSheetChallenge.UI
+{
+	/// <summary>
+	/// A generic implementation of <see cref="ICommand"/> that can be used to execute commands in
+	/// a view model.
+	/// </summary>
+	public class Command : ICommand
+	{
+		#region Events
+		/// <summary>
+		/// Triggered when the result of <see cref="CanExecute(object)"/> will change.
+		/// </summary>
+		public event EventHandler CanExecuteChanged;
+		#endregion
+
+		#region Properties
+		private Action<object> ExecuteAction { get; }
+
+		private Func<object, bool> CanExecuteFunction { get; }
+		#endregion
+
+		#region Constructor
+		/// <summary>
+		/// Instantiates a new <see cref="Command"/> object.
+		/// </summary>
+		/// <param name="execute">The action to execute when the command is triggered.</param>
+		/// <param name="canExecute">A function that returns true when the command can be executed
+		/// and false otherwise.</param>
+		public Command(Action<object> execute, Func<object, bool> canExecute = null)
+		{
+			this.ExecuteAction = execute;
+			this.CanExecuteFunction = canExecute ?? new Func<object, bool>(p => true);
+		}
+		#endregion
+
+		#region Public Methods
+		/// <summary>
+		/// Returns true when <see cref="Execute(object)"/> can be called and false otherwise.
+		/// </summary>
+		/// <param name="parameter">The parameter.</param>
+		public bool CanExecute(object parameter)
+		{
+			return this.CanExecuteFunction(parameter);
+		}
+
+		/// <summary>
+		/// Executed when the command is triggered.
+		/// </summary>
+		/// <param name="parameter">The parameter.</param>
+		public void Execute(object parameter)
+		{
+			this.ExecuteAction(parameter);
+		}
+
+		/// <summary>
+		/// Triggers the <see cref="CanExecuteChanged"/> event.
+		/// </summary>
+		public void OnCanExecuteChanged()
+		{
+			this.CanExecuteChanged?.Invoke(this, new EventArgs());
+		}
+		#endregion
+	}
+}
---
+++ AppSheetChallenge.UI/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using AppSheetChallenge.Library;
+
+namespace AppSheetChallenge.UI
+{
+	/// <summary>
+	/// The main window view model.
+	/// </summary>
+	public class MainViewModel : INotifyPropertyChanged
+	{
+		#region Class Variables
+		private ObservableCollection<Person> myPeople;
+		private Command myRetrieveAllCommand;
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// Gets the collection of people being displayed.
+		/// </summary>
+		public ObservableCollection<Person> People =>
+			myPeople ?? (myPeople = new ObservableCollection<Person>());
+
+		/// <summary>
+		/// Gets a command to retrieve all people.
+		/// </summary>
+		public Command RetrieveAllCommand =>
+			myRetrieveAllCommand ?? (myRetrieveAllCommand = new Command(this.RetrieveAll, p => this.ButtonsEnabled));
+
+		private bool ButtonsEnabled { get; set; } = true;
+		#endregion
+
+		#region Private Methods
+		private async void RetrieveAll(object parameter)
+		{
+			this.ButtonsEnabled = false;
+			this.RetrieveAllCommand.OnCanExecuteChanged();
+			this.People.Clear();
+			var finder = new PeopleFinder("https://appsheettest1.azurewebsites.net/sample");
+			foreach (var person in await finder.FindAllPeopleSortedByAge())
+			{
+				this.People.Add(person);
+			}
+			this.ButtonsEnabled = true;
+			this.RetrieveAllCommand.OnCanExecuteChanged();
+		}
+		#endregion
+
+		#region INotifyPropertyChanged Members
+		/// <summary>
+		/// Event that is triggered when a property value changes.
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// Triggers the <see cref="PropertyChanged"/> event.
+		/// </summary>
+		/// <param name="propertyName">The property name or the calling member name when unspecified.</param>
+		protected void OnPropertyChanged([CallerMemberName]string propertyName = "")
+		{
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+		#endregion
+	}
+}
